1bd30c911c3655ea61776fe09c84e47952cd57f4
roman-dudko <53345792+roman-dudko@users.noreply.github.com>
2021-01-13 19:56:17 +0300

    added test

---
+++ test_items.py
@@ -0,0 +1,6 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_verify_add_to_basket_button(browser):
+    browser.get(link)
+    assert len(browser.find_elements_by_css_selector(".btn-add-to-baskets")) > 0, "'Add to cart' button is not present!"
